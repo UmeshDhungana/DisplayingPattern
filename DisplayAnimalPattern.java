172f9724e85f75d4d094369abe9865320acdd5cd
UmeshDhungana <dumesh77@gmail.com>
2015-01-21 16:39:43 +0545

    file Added

---
+++ DisplayAnimalPattern.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Umesh on 1/16/2015.
+ */
+public class DisplayAnimalPattern {
+    public static void main(String[] args){
+        System.out.println("\t\t\t\t\t'__'");
+        System.out.println("\t\t\t\t\t(\u00A9\u00A9)");
+        System.out.println("\t\t\t/========\\/");
+        System.out.println("\t\t   / || %% ||");
+        System.out.println("\t\t  *  ||----||");
+        System.out.println("\t\t\t \u00A5\u00A5    \u00A5\u00A5  ");
+        System.out.println("\t\t\t \"\"    \"\"");
+    }
+}
